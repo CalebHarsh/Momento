a9201ab7cefd2b50aff89d777cea861e45f4664b
Dillon Schultz <dillonschultz93@gmail.com>
2018-03-29 20:49:01 -0700

    worked on navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,9 +1,48 @@
 import React, {Component} from 'react';
+import {Button, Divider} from 'antd';
+import Login from '../Login/Login'
+import 'antd/dist/antd.css';
+import './Navbar.css'
 
 class Navbar extends  Component {
+  constructor(props){
+    super(props);
+    this.handleSignIn = this.handleSignIn.bind(this)
+    this.state = {
+      visible: false
+    }
+  }
+  handleSignIn() {
+    this.setState((prevState) => {
+      return {
+        visible: !prevState.visible
+      }
+    })
+  }
   render() {
     return(
-      <p>Hello</p>
+      <div className="Navbar">
+        <div className="container">
+          <div className="logo-container">
+            <div className="logo"></div>
+            <h1 className="logotype">momento</h1>
+          </div>
+          <div className="nav-items">
+            {
+              this.state.visible && <Login className="fadeInRight"/>
+            }
+            <Button size="default"
+              onClick={this.handleSignIn}>
+              Sign In
+            </Button>
+            <Divider type="vertical" />
+            <Button size="default"
+              type="primary">
+              Sign Up
+            </Button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
