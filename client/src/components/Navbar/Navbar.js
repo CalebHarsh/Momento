d2448b2265cf42354e4460f5239b952a3d14ea93
Dillon Schultz <dillonschultz93@gmail.com>
2018-03-31 10:16:01 -0700

    added a logo asset

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import {Button, Divider} from 'antd';
 import Login from '../Login/Login'
 import 'antd/dist/antd.css';
 import './Navbar.css'
+import logo from '../../assets/images/logo.svg'
 
 class Navbar extends  Component {
   constructor(props){
@@ -24,12 +25,14 @@ class Navbar extends  Component {
       <div className="Navbar">
         <div className="container">
           <div className="logo-container">
-            <div className="logo"></div>
+            <div className="logo">
+              <img src={logo} />
+            </div>
             <h1 className="logotype">momento</h1>
           </div>
           <div className="nav-items">
             {
-              this.state.visible && <Login className="fadeInRight"/>
+              this.state.visible && <Login />
             }
             <Button size="default"
               onClick={this.handleSignIn}>
