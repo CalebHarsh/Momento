a867edea0de73cdd653e06f874e367e6fa254216
Trevor <trevcj@gmail.com>
2018-04-03 21:10:46 -0700

    false

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
+<<<<<<< Updated upstream
 import LandingPage from './pages/LandingPage'
 import SignUp from './pages/SignUp'
 class App extends Component {
@@ -12,6 +13,21 @@ class App extends Component {
           <Route exact path="/signup" component={SignUp} />
         </div>
       </Router>
+=======
+import Navbar from './components/Navbar/Navbar.js';
+import Albums from './components/Photo-Components/Albums';
+import Photos from './components/Photo-Components/Photos';
+import {Affix} from 'antd'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+          <Navbar />
+          <Albums />
+
+      </div>
+>>>>>>> Stashed changes
     );
   }
 }
