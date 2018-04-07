dad21e24f56c29b84dd2e09ea57017ab72bc482f
Dillon Schultz <dillonschultz93@gmail.com>
2018-04-06 18:47:00 -0700

    Looped through photo and album components

--- client/src/components/Album-Square/Album-Square.js
+++ client/src/components/Album-Square/Album-Square.js
@@ -6,14 +6,13 @@ const { Meta } = Card;
 
 const Square = props => (
   <Card
-    
-    style={{width: "100%"}}
-    cover={<img alt="example" src="https://az616578.vo.msecnd.net/files/2016/04/22/635969539404389959-525985018_chicken_fingers_page-bg_8285.jpg" />}
+    hoverable
+    cover={<img alt="example" src={props.src} />}
     actions={[<Icon type="setting" />, <Icon type="edit" />]}
   >
     <Meta
-      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
-      title="Chicken Tenders"
+      avatar={<Avatar icon="user" />}
+      title={props.title}
       description="This is the description"
     />
   </Card>
