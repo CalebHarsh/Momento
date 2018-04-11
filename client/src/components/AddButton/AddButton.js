10b94aea51907f912b5b7e31be4c50e8e919700b
Trevor <trevcj@gmail.com>
2018-04-10 19:01:25 -0700

    add button changes

--- client/src/components/AddButton/AddButton.js
+++ client/src/components/AddButton/AddButton.js
@@ -5,6 +5,9 @@ import PicUpload from "../PicUpload/PicUpload.js";
 const FormItem = Form.Item;
 
 class AddButton extends React.Component {
+
+
+
   componentWillMount() {
     var page = window.location.pathname;
     console.log(page, "this page");
@@ -14,45 +17,66 @@ class AddButton extends React.Component {
     });
     console.log(this.state.album, "checkPath");
   }
+
+  uploadPhoto = (picture) => {
+    console.log(picture, "this function is passing through");
+  }
+
   state = {
     visible: false,
     confirmLoading: false,
-    album: false
+    album: false,
+    img: "",
+    name: "",
+    description: ""
   }
+
   showModal = () => {
-    this.setState({visible: true});
+    this.setState({state: this.state.visible = true});
   }
   handleOk = () => {
     this.setState({confirmLoading: true});
     setTimeout(() => {
       this.setState({visible: false, confirmLoading: false});
     }, 1000);
-
   }
   handleCancel = () => {
     console.log('Clicked cancel button');
     this.setState({visible: false});
   }
+  itemName = () => {
+    this.setState({
+      name:
+    })
+    console.log("Name is working");
+  }
+
+  itemDesription = () => {
+    console.log("Description is also working");
+  }
+
   render() {
     const {visible, confirmLoading, ModalText} = this.state;
     var title = (this.state.album ? "Album Upload" : "Photo Upload");
     var name = (this.state.album ? "album" : "photo");
     var hidden = (this.state.album ? "hidden" : "show")
-    return (<div>
+    return (<div style={{zIndex: 9999}}>
       <Button style={{width: 60, height: 60, borderRadius: 55, position: "fixed", bottom: 0, right: 0, margin: "2rem"}} type="primary" onClick={this.showModal}>
         <Icon style={{color: "white", fontSize: 32, display: "flex", justifyContent: "center"}} type="plus" />
       </Button>
       <Modal title= {title} visible={visible} onOk={this.handleOk} confirmLoading={confirmLoading} onCancel={this.handleCancel}>
         <Form layout="vertical">
           <FormItem label="Name It!">
-            <Input type="text" placeholder="Foo Bar" name={name + "name"}/>
+            <Input onChange={this.itemName} type="text" placeholder="Foo Bar" name={name + "name"}/>
           </FormItem>
           <FormItem label="Add a description.">
-            <Input type="text" placeholder="Some more Foo Bar" name={name + "description"}/>
+            <Input onChange={this.itemDesription} type="text" placeholder="Some more Foo Bar" name={name + "description"}/>
           </FormItem>
         </Form>
         <div style={{visibility: hidden}}>
-          <PicUpload />
+          <PicUpload
+            onPicUpload = {this.uploadPhoto}
+          />
         </div>
       </Modal>
     </div>);
