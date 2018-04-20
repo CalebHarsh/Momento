b7d08593640223d16e11cf94bc052061885481a9
Dillon Schultz <dillonschultz93@gmail.com>
2018-04-19 21:58:51 -0700

    made the sign up page responsive

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -72,20 +72,24 @@ class SignUp extends Component {
       <div className="signup-page">
         <div className="content">
           <Row gutter={48}>
-            <Col md={{ span: 12 }}>
-              <div className="signupHook">
-                <h1 className="heading">Welcome to Momento</h1>
-                <h3 className="hookMsg">We are a micro social network based
-                  on sharing photos between friends, families, and significant
-                  others. Store your photos on our platform so your inner
-              circle stays in the loop.
-                </h3>
-                <p><Icon type="upload" className="icon" />Upload your images</p>
-                <p><Icon type="folder-add" className="icon" />Create albums</p>
-                <p><Icon type="usergroup-add" className="icon" />Add collaborators to your albums</p>
+            <Col lg={{ span: 12 }} md={{ span: 24 }}>
+              <div className="topSection">
+                <div className="signupHook">
+                  <h1 className="heading">Welcome to Momento</h1>
+                  <h3 className="hookMsg">We are a micro social network based
+                    on sharing photos between friends, families, and significant
+                    others. Store your photos on our platform so your inner
+                circle stays in the loop.
+                  </h3>
+                  <div className="icon-set">
+                    <p><Icon type="upload" className="icon" />Upload your images</p>
+                    <p><Icon type="folder-add" className="icon" />Create albums</p>
+                    <p><Icon type="usergroup-add" className="icon" />Add collaborators to your albums</p>
+                  </div>
+                </div>
               </div>
             </Col>
-            <Col md={{ span: 12 }}>
+            <Col lg={{ span: 12 }} md={{ span: 24 }}>
               <h1 className="heading">Sign Up</h1>
               <SignupForm
                 name={this.state.name}
